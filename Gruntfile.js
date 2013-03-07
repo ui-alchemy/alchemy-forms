2a44230605705e99fa4ea8b0d60a9fac18f0580a
Eric D Helms <ehelms@redhat.com>
2013-03-07 14:50:58 -0500

    Updating Gruntfile to remove generation of alchemy-forms.scss.

--- Gruntfile.js
+++ Gruntfile.js
@@ -113,9 +113,6 @@ module.exports = function (grunt) {
       },
       dist: {
         files: {
-          'dist/<%= yeoman.component %>.scss' : [
-            'component/styles/**/*.scss'
-          ]
         }
       },
       server: {
